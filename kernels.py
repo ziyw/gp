742b642b830707d63a0be5f2c9c2117993483f7d
Ziyan Wang <zywong159@gmail.com>
2017-08-30 10:14:41 -0400

    New kernels

--- kernels.py
+++ kernels.py
@@ -21,60 +21,54 @@ class Kernel:
 		self.K = K
 		return K
 
-
 	def RQ(self, output_scale, input_scale, index):
-		pass 
+		self.output_scale = output_scale
+		self.input_scale = input_scale 
+		self.index = index 
 
 	def cal_RQ(self,X):
-		pass 
+		X = X * 1.
+		h = self.output_scale
+		alpha = self.index
+		l = self.input_scale
+
+		R = np.power(X.T - X,2)
+		R = R / (alpha * l * l)
+		R = h*h * (R + 1)
+		
+		K = np.power(R, -alpha)
+
+		self.K = K
+		return K
 
 	def per_SE(self, output_scale, time_period, length_scale):
-		pass 
+		self.output_scale = output_scale
+		self.time_period = time_period
+		self.length_scale = length_scale
 
 	def cal_per_SE(self,X):
-		pass 
-
-
-# 	#def RQ(self, h, alpha, l):
-# 		"""
-# 		Rational quadratic 
-# 		input arguments:
-# 			h: output amplitude 
-# 			alpha : index 
-# 			l : input scale 
-# 		output:	
-# 			h^2(1 + (x_i - x_j) ^2 / (alpha * l^2))^(-alpha)
-# 		"""
-# 		self.h = h
-# 		self.alpha = alpha 
-# 		self.l = l
-
-# 		X = self.time
-# 		R = np.power(X.T - X,2)
-# 		R = R / (alpha * l * l)
-# 		R = h*h * (R + 1)
+		X = X * 1.
+		h = self.output_scale 
+		T = self.time_period
+		w = self.length_scale
 		
-# 		K = np.power(R, -alpha)
-
-# 		self.K = K
-# 		return K
-
-# 	##def per_SE(self, h,w,l):
-
-# 		X = self.time
-# 		R = np.pi * np.abs((X.T - X)/ l)
-# 		R = np.sin(R)
-# 		R = (1. / (2 * w * w)) * np.power(R, 2)
-# 		K = np.power(h,2) * np.exp(-R)
-# 		self.K = K
-
-# 		return K
-
-# # ker = Kernel.SE(1,1)
-
+		R = np.pi * np.abs((X.T - X)/ T)
+		R = np.sin(R)
+		R = (1. / (2 * w * w)) * np.power(R, 2)
+		K = np.power(h,2) * np.exp(-R)
+		
+		return K 
 
 if __name__ == '__main__':
-	ker = Kernel()
-	ker.SE(1,1)
 	x = np.array([1,2,3,4]).reshape(-1,1)
+	ker = Kernel()
+	
+	ker.SE(1,1)
 	print ker.cal_SE(x)
+
+	ker.RQ(1,2,3)
+	print ker.cal_RQ(x)
+
+
+	ker.per_SE(1,2,3)
+	print ker.cal_per_SE(x)
