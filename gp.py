5b41fef37e1f2010d872dc4766ee343708b90d88
Ziyan Wang <zywong159@gmail.com>
2017-09-07 17:37:33 -0400

    Marginal likelihood is wrong

--- gp.py
+++ gp.py
@@ -8,11 +8,7 @@ from scipy.linalg import solve
 from scipy.spatial.distance import pdist, squareform,euclidean
 from matplotlib import pyplot as plt
 
-# for
-import GPy
-
 class GP:
-
 
 	def __init__(self, time_points, values, kernel, mean = 0):
 		
@@ -25,23 +21,21 @@ class GP:
 		self.kers = []
 		self.kers.append(kernel)
 
-
 		if kernel.type == "SE":
 			kernel.cal_SE(time_points.reshape(-1,1))
 		
 		self.num_kernel = len(self.kers)
 
+	def optimize(self,time_point, value):
+		'''
+		optimize the function according to the likelihood function?
+		'''
 
-	def test():
-		"""
-		A test function to compare GP with GPY
-		"""
+		scipy.optimize.fmin_bfgs(get_likelihood(), [2, 2], fprime=fprime)
 		pass 
 
-	# optimize() auto gradient 
-
-	def get_likelihood(self, time_point, value):
-		return self.GPR(time_point, value, self.kernel)
+	def get_likelihood(self, predict_point):
+		return self.GPR(predict_point)[2]
 		
 	def plot(self):
 		'''
@@ -53,9 +47,8 @@ class GP:
 		axis_x = np.arange(range_min-1,range_max+1,0.1)
 		fig = plt.figure(0)
 
-
 		# background color
-		#plt.axis([0,5,-2,2], facecolor = 'g')
+		# plt.axis([0,5,-2,2], facecolor = 'g')
 		# plt.grid(color='w', linestyle='-', linewidth=0.5)
 		# ax.patch.set_facecolor('#E8E8F1')
 		
@@ -77,15 +70,15 @@ class GP:
 		plt.xlabel('Time points')
 		plt.ylabel('Values')
 		# show the points
-		plt.scatter(self.time_points, self.values,color = '#598BEB')
+		plt.scatter(self.time_points, self.values,color = '#598BEB', marker = 'X')
 		plt.show()
-
 
 	# a way compare GPs
 	def plot_compare(self,other_GPs):
 		# other_GPs should be a vector of GPs
 		# each one of them should be showed in the plot
 		# mu and scatter should be in different colors 
+		# change to different scatter 
 		GPs = [self, other_GPs]
 
 		plt.xlabel('Time points')
@@ -99,7 +92,6 @@ class GP:
 		range_min = np.min(self.time_points)
 		range_max = np.max(self.time_points)
 
-
 		for i in range(len(GPs)):
 			tmin = np.min(GPs[i].time_points)
 			tmax = np.max(GPs[i].time_points)
@@ -107,7 +99,6 @@ class GP:
 			range_max = max(range_max, tmax)
 		
 		axis_x = np.arange(range_min-1,range_max+1,0.1)
-		print axis_x
 		fig = plt.figure(0)
 		ax = fig.add_subplot(111)
 		ax.spines['top'].set_visible(False)
@@ -121,9 +112,9 @@ class GP:
 			
 			for j in range(axis_x.size):
 				mu[j],var[j],_ = GPs[i].GPR(predict_point = axis_x[j])
-			
-			print mu 
-			print var
+			 
+			# print mu 
+			# print var
 			plt.fill_between(axis_x,mu + var,mu-var,color = cov_color[i % k_color],alpha = 0.5)
 			
 			# show mean 
@@ -131,22 +122,18 @@ class GP:
 
 
 		# background color
-		#plt.axis([0,5,-2,2], facecolor = 'g')
+		# plt.axis([0,5,-2,2], facecolor = 'g')
 		# plt.grid(color='w', linestyle='-', linewidth=0.5)
 		# ax.patch.set_facecolor('#E8E8F1')
-
 		# show the points
 		for i in range(len(GPs)):
-			plt.scatter(GPs[i].time_points, GPs[i].values,color = mean_color[i % k_color])
+			plt.scatter(GPs[i].time_points, GPs[i].values,color = mean_color[i % k_color], marker = 'X')
 		
 		plt.show()
 
-
-
 		pass
 
-	def GPR(self,predict_point,noise_level = 1):
-
+	def GPR(self,predict_point,noise_level = 0):
 
 		if self.num_kernel == 1:
 			K = self.kers[0].K
@@ -193,8 +180,10 @@ class GP:
 		var = cov_k - np.matmul(v.T, v)
 
 		# marginal likelihood 
+		# likelihood is wrong
 		p = - 1.0 / 2.0 * np.dot(y , alpha) - np.sum(np.log(L.diagonal())) - (N/2.0 *  np.log (2 * np.pi))
 
+		print p
 		return mean, var, p 
 
 	def add_kernel(self, new_kernel):
@@ -208,22 +197,11 @@ class GP:
 
 	def set_kernels(self, new_kernels):
 		'''
-		Set all kernels to a new set 
+		Set all kernels to a new set
+
 		'''
+
+
+
 		pass
-if __name__ == '__main__':
 
-	# format to use GP
-	k1 = Kernel("SE",1,1)
-	k2 = Kernel("SE",2,3)
-
-	t1 = np.array([1,2,3,4])
-	t2 = np.array([3,4,5,6])
-	v = np.sin(t1) + np.random.normal(0,1,4)
-
-	predict_points = np.array([5])
-
-	gp1 = GP(time_points = t1, values = v, kernel = k1, mean = 0)
-	gp2 = GP(time_points = t2, values = v, kernel = k2, mean = 0)
-
-	gp1.plot_compare(gp2)
